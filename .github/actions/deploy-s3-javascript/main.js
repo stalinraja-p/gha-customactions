e1029afec693654a2d73cb3593972a1bc3866d87
“Stalin <stalinraja.aws@gmail.com>
2026-02-27 21:21:20 +0530

    custom action deploy aws

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,11 +1,25 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 
 function run() {
-    core.notice('Hello from my custom JavaScript action!');
+    //1. Get some input values:
+
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+    
+    // 2. Upload files to S3:
+    const s3Uri = `s3://${bucket}`;
+    
+
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+    
+    core.notice('Hello from my custom JavaScript Action!'); 
+    
 }
+
 
 run();
 
